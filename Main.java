d3ec4e475acb9427823027349d3fa9c4b41beb9e
Ahnchihyuk <clgurdl99@gmail.com>
2024-10-16 09:41:11 +0900

    five

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("안치혁");
         World.run();
+        World.run2();
         Hello.run();
     }
 
--- World.java
+++ World.java
@@ -4,4 +4,8 @@ public class World {
         System.out.println("201814005");
     }
     
+    public static void run2() {
+        System.out.println("hello world");
+    }
+
 }
